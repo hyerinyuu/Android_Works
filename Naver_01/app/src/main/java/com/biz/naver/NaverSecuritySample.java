ae92414588f144b8e68c815485590fb5256c3b00
hyerinyuu <hyerin.you@gmail.com>
2020-02-24 09:21:31 +0900

    naver api

---
+++ Naver_01/app/src/main/java/com/biz/naver/NaverSecuritySample.java
@@ -0,0 +1,7 @@
+package com.biz.naver;
+
+public class NaverSecuritySample {
+
+    public final static String NAVER_SEC = "YOUR NAVER CLIENT ID";
+    public final static String NAVER_ID = "YOUR NAVER SECRET";
+}
